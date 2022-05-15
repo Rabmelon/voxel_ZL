12957250cddbde63dbf6f72f9f99e9ad701e086c
ZLei <zhibinlei@outlook.com>
2022-05-15 17:03:54 +0200

    finish the main cubic body

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ from taichi.math import *
 scene = Scene(voxel_edges=0, exposure=2)    # create a scene, set the width of voxel edge line and exposure value
 scene.set_floor(-1, (0.5, 0.5, 0.4))     # height and color of floor
 scene.set_background_color((0.5, 0.5, 0.4))     # color of sky
-scene.set_directional_light((1, 1, -1), 0.2, (1, 0.8, 0.6))     # direction and color of light
+scene.set_directional_light((1, 1, 1), 0.2, (1, 0.8, 0.6))     # direction and color of light
+dir_face = (vec3(1,0,0), vec3(0,1,0), vec3(0,0,1), vec3(-1,0,0), vec3(0,-1,0), vec3(0,0,-1))
+dir_corner = (vec3(1,1,1), vec3(1,1,-1), vec3(1,-1,1), vec3(1,-1,-1), vec3(-1,1,1), vec3(-1,1,-1), vec3(-1,-1,1), vec3(-1,-1,-1))
+dir_edge = (vec3(1,1,0), vec3(1,-1,0), vec3(-1,1,0), vec3(-1,-1,0), vec3(1,0,1), vec3(1,0,-1), vec3(-1,0,1), vec3(-1,0,-1), vec3(0,1,1), vec3(0,1,-1), vec3(0,-1,1), vec3(0,-1,-1))
 
 @ti.func
 def create_block(pos, size, color, color_noise):
@@ -19,25 +22,13 @@ def create_block(pos, size, color, color_noise):
         scene.set_voxel(I, 1, color + color_noise * ti.random())
 
 @ti.func
+def extend_center(cpos, direction, length, width, color, color_noise):
+    begin_pos = cpos - direction * length / 2 - (vec3(1) - direction) * width // 2
+    size = direction * length + (vec3(1) - direction) * width
+    create_block(begin_pos, size, color, color_noise)
+
+@ti.func
 def create_line_centroid(start, direction, length, width, color, color_noise):
-    pass
-
-@ti.func
-def create_cubic_frame(center, d, w, color, color_noise):
-    # 先根据中心点、边长、厚度，确定12个边的起点、方向、尺寸
-    pass
-
-@ti.func
-def create_cubic_edge(start, direction, length, width):
-    # 根据中心起点、方向、长度、厚度，构造体素边（不包含角点）
-    pass
-
-@ti.func
-def create_cubic_corner(center, d, w):
-    pass
-
-@ti.func
-def create_cubic_face():
     pass
 
 @ti.func
@@ -48,13 +39,27 @@ def create_arrow(start, direction, length, color):
 @ti.kernel
 def initialize_voxels():
     # scene.set_voxel(vec3(0, 0, 0), 1, vec3(1))  # Add a white(1,1,1) voxel at (0,0,0), property is 1 solid voxel or 2 light source voxel
-    create_block(ivec3(-64, 63, -64), ivec3(128, 1, 1), vec3(1.0, 0.0, 0.0), vec3(0.1))
+    # create_block(ivec3(-64, 63, -64), ivec3(128, 1, 1), vec3(1.0, 0.0, 0.0), vec3(0.1))
     create_block(ivec3(-64, -64, -64), ivec3(128, 1, 1), vec3(1.0, 0.0, 0.0), vec3(0.1))
-    create_block(ivec3(-64, -64, 63), ivec3(128, 1, 1), vec3(1.0, 0.0, 0.0), vec3(0.1))
+    # create_block(ivec3(-64, -64, 63), ivec3(128, 1, 1), vec3(1.0, 0.0, 0.0), vec3(0.1))
     create_block(ivec3(-64, -64, -64), ivec3(1, 128, 1), vec3(0.0, 1.0, 0.0), vec3(0.1))
-    create_block(ivec3(-64, 63, -64), ivec3(1, 1, 128), vec3(0.0, 0.0, 1.0), vec3(0.1))
+    # create_block(ivec3(-64, 63, -64), ivec3(1, 1, 128), vec3(0.0, 0.0, 1.0), vec3(0.1))
     create_block(ivec3(-64, -64, -64), ivec3(1, 1, 128), vec3(0.0, 0.0, 1.0), vec3(0.1))
-    create_block(ivec3(63, -64, -64), ivec3(1, 1, 128), vec3(0.0, 0.0, 1.0), vec3(0.1))
+    # create_block(ivec3(63, -64, -64), ivec3(1, 1, 128), vec3(0.0, 0.0, 1.0), vec3(0.1))
+
+    center = vec3(0, 0, 0)
+    l_cubic = 60
+    w_face = 2
+    w_edge = 6
+    color_face = vec3(0.2, 0.4, 0.6)
+    color_edge = vec3(0.2, 0.2, 0.2)
+    color_corner = vec3(0.8, 0.8, 0.8)
+    for i in ti.static(range(len(dir_face))):
+        extend_center(center + dir_face[i] * l_cubic // 2, vec3(1)-ti.abs(dir_face[i]), l_cubic - w_edge, w_face, color_face, vec3(0.1))
+    for i in ti.static(range(len(dir_edge))):
+        extend_center(center + dir_edge[i] * l_cubic // 2, vec3(1)-ti.abs(dir_edge[i]), l_cubic - w_edge, w_edge, color_edge, vec3(0.1))
+    for i in ti.static(range(len(dir_corner))):
+        extend_center(center + dir_corner[i] * l_cubic // 2, vec3(1)-ti.abs(dir_corner[i]), w_edge, w_edge, color_corner, vec3(0.1))
 
 initialize_voxels()
 
